9a2fb61e4c6312cacdf178149815ac0c07a67696
contactcomenzar-rgb <contactcomenzar@gmail.com>
2026-03-20 23:51:16 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,10 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ["http://127.0.0.1:5500", "http://localhost:5500","http://localhost:5000"],
+  origin: [
+    process.env.CLIENT_URL,
+    "http://127.0.0.1:5500"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 window.CONFIG = {
-    API_BASE_URL: "http://localhost:5000/api"
     // API_BASE_URL: "http://localhost:5000/api"
+    API_BASE_URL: "srv-d6uorbdm5p6s7392270g"
 };
